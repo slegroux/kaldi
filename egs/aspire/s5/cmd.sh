a89f6a231f6532d133c2475289b7761c5ba3d4e7
Sylvain Le Groux <sylvainlg@voicera.ai>
2019-08-14 13:39:05 -0700

    run.pl

--- egs/aspire/s5/cmd.sh
+++ egs/aspire/s5/cmd.sh
@@ -1,4 +1,4 @@
-# "queue.pl" uses qsub.  The options to it are
+# "run.pl" uses qsub.  The options to it are
 # options to qsub.  If you have GridEngine installed,
 # change this to a queue you have access to.
 # Otherwise, use "run.pl", which will run jobs locally
@@ -6,19 +6,19 @@
 # the number of cpus on your machine.
 
 #a) JHU cluster options
-export train_cmd="queue.pl"
-export decode_cmd="queue.pl --mem 4G"
-export mkgraph_cmd="queue.pl --mem 8G"
+export train_cmd="run.pl"
+export decode_cmd="run.pl --mem 4G"
+export mkgraph_cmd="run.pl --mem 8G"
 
 
 #b) BUT cluster options
-#export train_cmd="queue.pl -q all.q@@blade -l ram_free=1200M,mem_free=1200M"
-#export decode_cmd="queue.pl -q all.q@@blade -l ram_free=1700M,mem_free=1700M"
-#export decodebig_cmd="queue.pl -q all.q@@blade -l ram_free=4G,mem_free=4G"
+#export train_cmd="run.pl -q all.q@@blade -l ram_free=1200M,mem_free=1200M"
+#export decode_cmd="run.pl -q all.q@@blade -l ram_free=1700M,mem_free=1700M"
+#export decodebig_cmd="run.pl -q all.q@@blade -l ram_free=4G,mem_free=4G"
 
-#export cuda_cmd="queue.pl -q long.q@@pco203 -l gpu=1"
-#export cuda_cmd="queue.pl -q long.q@pcspeech-gpu"
-#export mkgraph_cmd="queue.pl -q all.q@@servers -l ram_free=4G,mem_free=4G"
+#export cuda_cmd="run.pl -q long.q@@pco203 -l gpu=1"
+#export cuda_cmd="run.pl -q long.q@pcspeech-gpu"
+#export mkgraph_cmd="run.pl -q all.q@@servers -l ram_free=4G,mem_free=4G"
 
 #c) run it locally...
 #export train_cmd=run.pl
