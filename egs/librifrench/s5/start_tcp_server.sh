7e191b32ac42d124424dcc868b07629d1fb77755
Sylvain Le Groux <slegroux@ccrma.stanford.edu>
2020-06-30 00:32:39 +0000

    update path to dx05 setup

--- egs/librifrench/s5/start_tcp_server.sh
+++ egs/librifrench/s5/start_tcp_server.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 samp_freq=16000
-model_dir=/home/sylvainlg/kaldi/egs/librifrench/s5/exp/chain_online_cmn/tdnn1k_sp_online
+model_dir=$(pwd)/exp/chain_online_cmn/tdnn1k_sp_online
 model=$model_dir/final.mdl
-graph=/home/sylvainlg/kaldi/egs/librifrench/s5/exp/chain_online_cmn/tree_sp/graph_tgsmall/HCLG.fst
-words=/home/sylvainlg/kaldi/egs/librifrench/s5/exp/chain_online_cmn/tree_sp/graph_tgsmall/words.txt
+graph=$(pwd)/exp/chain_online_cmn/tree_sp/graph_tgsmall/HCLG.fst
+words=$(pwd)/exp/chain_online_cmn/tree_sp/graph_tgsmall/words.txt
+
 online_conf=$model_dir/conf/online.conf
 
 online2-tcp-nnet3-decode-faster \
